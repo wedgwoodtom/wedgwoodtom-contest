3f04331b8b4efe3800962ddcfb06469ce716e12f
tompatterson <wedgwood.tom@gmail.com>
2016-11-05 19:08:26 -0700

    Deleted obsolete Customer code

--- service/src/main/java/hello/Application.java
+++ service/src/main/java/hello/Application.java
@@ -12,8 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner
 {
-    @Autowired
-    private CustomerRepository customerRepository;
 
     public static void main(String[] args)
     {
@@ -23,32 +21,6 @@ public class Application implements CommandLineRunner
     @Override
     public void run(String... args) throws Exception
     {
-        customerRepository.deleteAll();
-
-        // save a couple of customers
-        customerRepository.save(new Customer("Alice", "Smith"));
-        customerRepository.save(new Customer("Bob", "Smith"));
-
-        // fetch all customers
-        System.out.println("Customers found with findAll():");
-        System.out.println("-------------------------------");
-        for (Customer customer : customerRepository.findAll())
-        {
-            System.out.println(customer);
-        }
-        System.out.println();
-
-        // fetch an individual customer
-        System.out.println("Customer found with findByFirstName('Alice'):");
-        System.out.println("--------------------------------");
-        System.out.println(customerRepository.findByFirstName("Alice"));
-
-        System.out.println("Customers found with findByLastName('Smith'):");
-        System.out.println("--------------------------------");
-        for (Customer customer : customerRepository.findByLastName("Smith"))
-        {
-            System.out.println(customer);
-        }
 
     }
 
